bfc12c9bbf64f5df2bd89e44be09057315314f84
Your Name <you@example.com>
2018-10-09 14:42:21 -0500

    cambios con frontend

--- pets/wsgi.py
+++ pets/wsgi.py
@@ -7,10 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+
+
+import os,sys,time,traceback,signal
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/sistemas/pets/DaleLaPata/pets')
+sys.path.append('/home/sistemas/pets/DaleLaPata/venv/lib/python3.5/site-packages') 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pets.settings.prod")
 
-application = get_wsgi_application()
+
+
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5) 
+
+
